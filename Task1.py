b5faa181713ceed711505eced8ebbf22fcefd500
Thanh Nguyen <71760329+Thanhibanani@users.noreply.github.com>
2024-02-07 21:28:24 +0100

    task1

---
+++ Task1.py
@@ -0,0 +1,19 @@
+
+
+def calc_jfi(throughputs):
+    num_flows = len(throughputs)
+    sum_throughputs = sum(throughputs)
+    sum_square = sum(x**2 for x in throughputs)
+    
+    if num_flows==0:
+        return 0
+    else:
+        jfi=(sum_throughputs**2)/ (num_flows *sum_square)
+        return jfi
+    
+if __name__ == "__main__":
+    throughputs =[float(throughputs) for throughputs in input("Enter throughputs").split()]
+    jfi =calc_jfi(throughputs)
+    print("jfi:" ,jfi)
+    
+    
